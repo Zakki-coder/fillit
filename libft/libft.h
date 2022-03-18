4e4bd97fe55503b2eb46c9925fe24b0eb5a363cb
jniemine <jniemine@c1r3p5.hive.fi>
2022-03-18 16:04:17 +0200

    libft fix

--- libft/libft.h
+++ libft/libft.h
@@ -6,12 +6,14 @@
 /*   By: jniemine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 09:53:35 by jniemine          #+#    #+#             */
-/*   Updated: 2022/03/18 15:53:11 by jniemine         ###   ########.fr       */
+/*   Updated: 2022/03/18 16:02:49 by jniemine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+# include <string.h>
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
